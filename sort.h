8409d5f7640676aff2fc4116f94489ea5e593f28
xxfariddx <qarayevfarid3@gmail.com>
2026-02-07 11:54:50 +0000

    C

---
+++ sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/* Bubble sort algorithm prototype */
+void bubble_sort(int *array, size_t size);
+
+/* Helper function to print an array (for debugging) */
+void print_array(int *array, size_t size);
+
+#endif /* SORT_H */
